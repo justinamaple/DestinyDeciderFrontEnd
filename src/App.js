5957336f6dd4e1f0d63b3c92ccc2b9abdd354c40
Justin Maple <justinamaple@gmail.com>
2019-11-20 13:10:30 -0800

    Added prettier, set up basic Users/Lists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import logo from './assets/logo.svg'
+import './styles/App.css'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Lists from './containers/lists'
+import Users from './containers/users'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <h1>App</h1>
+        <Route path='/users' component={Users} />
+        <Route path='/lists' component={Lists} />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/lists.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+class Lists extends Component {
+  render() {
+    return <h1>Lists</h1>
+  }
+}
+export default Lists
