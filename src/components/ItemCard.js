954d262e4d2a12b2e6da0ebc9773f1a03600992d
Justin Maple <justinamaple@gmail.com>
2019-11-21 10:00:36 -0800

    Moved key to env

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,6 +3,7 @@ import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 
 const DESTINY2_URL = 'https://www.bungie.net'
@@ -10,10 +11,13 @@ const DESTINY2_URL = 'https://www.bungie.net'
 const useStyles = makeStyles(theme => ({
   card: {
     display: 'flex'
+    // width: 300,
+    // height: 150
   },
   details: {
     display: 'flex',
     flexDirection: 'column'
+    // width: 150
   },
   content: {
     flex: '1 0 auto'
@@ -33,18 +37,22 @@ function ItemCard(props) {
 
   return (
     <Card className={classes.card}>
-      <div className={classes.details}>
-        <CardContent className={classes.content}>
-          <Typography component='h5' variant='h5'>
-            {props.info.displayProperties.name}
-          </Typography>
-          <Typography variant='subtitle1' color='textSecondary'>
-            {props.info.itemTypeAndTierDisplayName}
-          </Typography>
-          <p>{props.info.displayProperties.description}</p>
-        </CardContent>
-      </div>
-      <CardMedia className={classes.cover} image={iconURL} />
+      <Grid container spacing={2}>
+        <Grid item xs={6} className={classes.details}>
+          <CardContent className={classes.content}>
+            <Typography component='h5' variant='h5'>
+              {props.info.displayProperties.name}
+            </Typography>
+            <Typography variant='subtitle1' color='textSecondary'>
+              {props.info.itemTypeAndTierDisplayName}
+            </Typography>
+            <p>{props.info.displayProperties.description}</p>
+          </CardContent>
+        </Grid>
+        <Grid item xs={6}>
+          <CardMedia className={classes.cover} image={iconURL} />
+        </Grid>
+      </Grid>
     </Card>
   )
 }
--- src/containers/Weapons.js
+++ src/containers/Weapons.js
@@ -4,51 +4,56 @@ import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import ItemCard from '../components/ItemCard'
 // TODO: This will need to be updated, maybe use a map that strores the base name to this versions hash value?
-import InventoryBucket from '../assets/manifests/DestinyInventoryBucketDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
-import InventoryItem from '../assets/manifests/DestinyInventoryItemDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
+// import InventoryBucket from '../assets/manifests/DestinyInventoryBucketDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
+// import InventoryItem from '../assets/manifests/DestinyInventoryItemDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
 
+// TODO: Don't hardcode these
 const DESTINY2_URL = 'https://www.bungie.net/Platform/Destiny2'
+const membershipType = '3'
+const membershipId = '4611686018467712948'
+const characterId = '2305843009304746410'
 
 class Weapons extends Component {
   state = {
     inventory: []
   }
 
-  fetchBungie = () => {
-    // TODO: Don't hardcode these
-    const accountId = '4611686018467712948'
-    const characterId = '2305843009304746410'
+  fetchCharacterEquipment = () => {
     // TODO: Find a way for this to work in PROD
-    const API_KEY = process.env.API_KEY
+    const API_KEY = process.env.REACT_APP_BUNGIE_API_KEY
 
     const config = {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        'X-Api-Key': '2f568106e32e4238919d0eb161cc15a2'
+        'X-Api-Key': API_KEY
       }
     }
 
     fetch(
-      `${DESTINY2_URL}/3/Profile/${accountId}/Character/${characterId}/?components=205`,
+      `${DESTINY2_URL}/${membershipType}/Profile/${membershipId}/Character/${characterId}/?components=205`,
       config
     )
       .then(resp => resp.json())
       .then(json => {
-        console.log(json)
         console.log(json.Response.equipment.data.items)
         this.setState({
           inventory: [...json.Response.equipment.data.items]
         })
+
+        this.fetchItemInstances()
       })
   }
+
+  fetchItemInstances = () => {}
 
   renderInventory = () => {
     return this.state.inventory.map(item => {
       // let bucket = InventoryBucket[this.convertHash(item.bucketHash)]
-      let invItem = InventoryItem[this.convertHash(item.itemHash)]
-      console.log(invItem)
+      // let invItem = InventoryItem[this.convertHash(item.itemHash)]
+      let invItem = null
+      // console.log(invItem)
       return invItem ? (
         <Grid key={item.itemInstanceId} item m={3} sm={4} xs={6}>
           <ItemCard info={invItem} />
@@ -64,7 +69,7 @@ class Weapons extends Component {
   }
 
   componentDidMount() {
-    this.fetchBungie()
+    this.fetchCharacterEquipment()
   }
 
   render() {
