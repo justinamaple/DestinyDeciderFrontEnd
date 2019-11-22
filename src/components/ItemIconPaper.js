e6278cfaa409201eb28103a24a4ffce25c1d9e82
Justin Maple <justinamaple@gmail.com>
2019-11-21 16:48:40 -0800

    Gets all weapons for an account and displays

---
+++ src/components/ItemIconPaper.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  }
+}))
+
+const DESTINY2_URL = 'https://www.bungie.net'
+
+export default function PaperSheet(props) {
+  const classes = useStyles()
+  const theme = useTheme()
+  const iconURL = props.info.displayProperties.hasIcon
+    ? `${DESTINY2_URL}${props.info.displayProperties.icon}`
+    : ''
+
+  return (
+    <Paper className={classes.root}>
+      <Grid item xs={12}>
+        <img src={iconURL} alt={props.info.displayProperties.name} />
+      </Grid>
+      {/* <Grid item xs={12}>
+        <Typography component='h6' variant='h6'>
+          {props.info.displayProperties.name}
+        </Typography>
+        <Typography variant='subtitle1' color='textSecondary'>
+          {props.info.itemTypeAndTierDisplayName}
+        </Typography>
+      </Grid> */}
+    </Paper>
+  )
+}
--- src/containers/Weapons.js
+++ src/containers/Weapons.js
@@ -3,22 +3,45 @@ import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import ItemCard from '../components/ItemCard'
+import ItemIconPaper from '../components/ItemIconPaper'
 // TODO: This will need to be updated, maybe use a map that strores the base name to this versions hash value?
-// import InventoryBucket from '../assets/manifests/DestinyInventoryBucketDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
-// import InventoryItem from '../assets/manifests/DestinyInventoryItemDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
+import InventoryBucket from '../assets/manifests/DestinyInventoryBucketDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
+import InventoryItem from '../assets/manifests/DestinyInventoryItemDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
+import ItemCategory from '../assets/manifests/DestinyItemCategoryDefinition-7ab91c74-e8a4-40c7-9f70-16a4354125c0.json'
 
 // TODO: Don't hardcode these
 const DESTINY2_URL = 'https://www.bungie.net/Platform/Destiny2'
-const membershipType = '3'
-const membershipId = '4611686018467712948'
-const characterId = '2305843009304746410'
+const membershipType = '3' // Guessing this is Steam?
+const membershipId = '4611686018467712948' // J3ubbleboy
+const characterId = '2305843009304746410' // Hunter
+
+const GeneralItemBucket = 138197802
+
+const WeaponBuckets = {
+  1498876634: 'Kinetic',
+  2465295065: 'Energy',
+  953998645: 'Power'
+}
+
+const DamageTypeBuckets = {
+  1: 'Kinetic',
+  3373582085: 'Kinetic',
+  2: 'Arc',
+  2303181850: 'Arc',
+  3: 'Solar',
+  1847026933: 'Solar',
+  4: 'Void',
+  3454344768: 'Void'
+}
 
 class Weapons extends Component {
   state = {
-    inventory: []
+    inventory: [],
+    profileWeapons: [],
+    weaponInstanceIds: []
   }
 
-  fetchCharacterEquipment = () => {
+  fetchCharactersEquippedWeapons = () => {
     // TODO: Find a way for this to work in PROD
     const API_KEY = process.env.REACT_APP_BUNGIE_API_KEY
 
@@ -32,31 +55,177 @@ class Weapons extends Component {
     }
 
     fetch(
-      `${DESTINY2_URL}/${membershipType}/Profile/${membershipId}/Character/${characterId}/?components=205`,
+      `${DESTINY2_URL}/${membershipType}/Profile/${membershipId}/?components=205`,
       config
     )
       .then(resp => resp.json())
       .then(json => {
-        console.log(json.Response.equipment.data.items)
-        this.setState({
-          inventory: [...json.Response.equipment.data.items]
-        })
+        console.log(json)
+        let allCharEquippedWeapons = []
+        let characters = json.Response.characterEquipment.data
 
-        this.fetchItemInstances()
+        for (let character in characters) {
+          let charEquippedWeapons = this.filterCharacterForWeapons(
+            characters[character]
+          )
+          allCharEquippedWeapons = [
+            ...allCharEquippedWeapons,
+            ...charEquippedWeapons
+          ]
+        }
+
+        this.setState(prevState => {
+          return {
+            profileWeapons: [
+              ...prevState.profileWeapons,
+              ...allCharEquippedWeapons
+            ]
+          }
+        })
       })
   }
 
-  fetchItemInstances = () => {}
+  fetchCharactersInventoryWeapons = () => {
+    // TODO: Find a way for this to work in PROD
+    const API_KEY = process.env.REACT_APP_BUNGIE_API_KEY
 
-  renderInventory = () => {
-    return this.state.inventory.map(item => {
-      // let bucket = InventoryBucket[this.convertHash(item.bucketHash)]
-      // let invItem = InventoryItem[this.convertHash(item.itemHash)]
-      let invItem = null
-      // console.log(invItem)
-      return invItem ? (
-        <Grid key={item.itemInstanceId} item m={3} sm={4} xs={6}>
-          <ItemCard info={invItem} />
+    const config = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'X-Api-Key': API_KEY
+      }
+    }
+
+    fetch(
+      `${DESTINY2_URL}/${membershipType}/Profile/${membershipId}/?components=201`,
+      config
+    )
+      .then(resp => resp.json())
+      .then(json => {
+        let allCharWeapons = []
+        let characters = json.Response.characterInventories.data
+
+        for (let character in characters) {
+          let charWeapons = this.filterCharacterForWeapons(
+            characters[character]
+          )
+          allCharWeapons = [...allCharWeapons, ...charWeapons]
+        }
+
+        this.setState(prevState => {
+          return {
+            profileWeapons: [...prevState.profileWeapons, ...allCharWeapons]
+          }
+        })
+      })
+  }
+
+  filterCharacterForWeapons = character => {
+    let weapons = []
+
+    character.items.forEach(item => {
+      if (WeaponBuckets[item.bucketHash]) {
+        let invItem = this.hashLookup(InventoryItem, item.itemHash)
+        invItem.itemCategoryHashes.forEach(itemCategoryHash => {
+          let itemCategory = this.hashLookup(ItemCategory, itemCategoryHash)
+          if (itemCategory.shortTitle === 'Weapon') {
+            weapons.push({
+              ...invItem,
+              itemInstanceId: item.itemInstanceId
+            })
+          }
+        })
+      }
+    })
+
+    return weapons
+  }
+
+  fetchProfileWeapons = () => {
+    // TODO: Find a way for this to work in PROD
+    const API_KEY = process.env.REACT_APP_BUNGIE_API_KEY
+
+    const config = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'X-Api-Key': API_KEY
+      }
+    }
+
+    fetch(
+      `${DESTINY2_URL}/${membershipType}/Profile/${membershipId}/?components=102`,
+      config
+    )
+      .then(resp => resp.json())
+      .then(json => {
+        let profileWeapons = this.filterProfileForWeapons(
+          json.Response.profileInventory
+        )
+
+        this.setState(prevState => {
+          return {
+            profileWeapons: [...prevState.profileWeapons, ...profileWeapons]
+          }
+        })
+      })
+  }
+
+  filterProfileForWeapons = inventory => {
+    let weapons = []
+
+    inventory.data.items.forEach(item => {
+      if (item.bucketHash === GeneralItemBucket) {
+        let invItem = this.hashLookup(InventoryItem, item.itemHash)
+        invItem.itemCategoryHashes.forEach(itemCategoryHash => {
+          let itemCategory = this.hashLookup(ItemCategory, itemCategoryHash)
+          if (itemCategory.shortTitle === 'Weapon') {
+            weapons.push({
+              ...invItem,
+              itemInstanceId: item.itemInstanceId
+            })
+          }
+        })
+      }
+    })
+
+    return weapons
+  }
+
+  fetchItemInstances = () => {
+    // TODO: Find a way for this to work in PROD
+    const API_KEY = process.env.REACT_APP_BUNGIE_API_KEY
+
+    const config = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'X-Api-Key': API_KEY
+      }
+    }
+
+    console.log(this.state)
+    this.state.weaponInstanceIds.forEach(instanceId => {
+      fetch(
+        `${DESTINY2_URL}/${membershipType}/Profile/${membershipId}/Item/${instanceId}/?components=300,302,304,305`,
+        config
+      )
+        .then(resp => resp.json())
+        .then(json => {
+          console.log(json.Response)
+        })
+    })
+  }
+
+  renderVaultWeapons = () => {
+    return this.state.profileWeapons.map(item => {
+      return item ? (
+        <Grid key={item.itemInstanceId} item xs={2}>
+          <ItemIconPaper info={item} />
         </Grid>
       ) : null
     })
@@ -68,15 +237,21 @@ class Weapons extends Component {
     return shift <= 0 ? i : shift
   }
 
+  hashLookup = (manifest, hash) => {
+    return manifest[this.convertHash(hash)]
+  }
+
   componentDidMount() {
-    this.fetchCharacterEquipment()
+    this.fetchCharactersInventoryWeapons()
+    this.fetchCharactersEquippedWeapons()
+    this.fetchProfileWeapons()
   }
 
   render() {
     return (
       <>
         <Grid container spacing={3}>
-          {this.renderInventory()}
+          {this.renderVaultWeapons()}
         </Grid>
       </>
     )
